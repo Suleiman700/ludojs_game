8969ad581e916efee2c26e185275d00a49b685db
Suleiman700 <soleman630@gmail.com>
2022-02-09 15:09:32 +0200

    Delete ludojs directory

--- ludojs/assets/js/base.js
+++
@@ -1,8 +0,0 @@
-function setGlowingBase(player_number) {
-    $(`#base_player0`).removeClass('glowing-base')
-    $(`#base_player1`).removeClass('glowing-base')
-    $(`#base_player2`).removeClass('glowing-base')
-    $(`#base_player3`).removeClass('glowing-base')
-
-    $(`#base_player${player_number}`).addClass('glowing-base')
-}
